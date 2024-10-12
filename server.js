1317a3a7ac98dad9a1a18616efab9d684bedccc0
Jet Chiang <jetjiang.ez@gmail.com>
2024-10-12 01:10:41 -0400

    Backend server express

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/cards", (req, res) => {
+  const filePath = path.join(__dirname, "cards.json");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading file");
+      return;
+    }
+    try {
+      const cards = JSON.parse(data);
+      const shuffledCards = cards.sort(() => 0.5 - Math.random());
+      const selectedCards = shuffledCards.slice(0, 5);
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(selectedCards));
+    } catch (parseErr) {
+      res.status(500).send("Error parsing JSON");
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
